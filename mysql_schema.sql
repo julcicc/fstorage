5529fc3e68f55a28d9f34e8f158232ad647fae38
Julian Ciccale <julian@imaweb.net>
2016-12-11 20:49:41 +0100

    Updated SQL Schema

--- mysql_schema.sql
+++ mysql_schema.sql
@@ -1,22 +1,18 @@
---
--- Table structure for table `buckets`
---
 CREATE TABLE `buckets` (
   `bucket_name` varchar(50) NOT NULL,
   `bucket_description` text,
   PRIMARY KEY (`bucket_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Table structure for table `objects`
---
 CREATE TABLE `objects` (
   `bucket_name` varchar(50) NOT NULL,
   `object_key` varchar(1000) NOT NULL,
   `date_created` datetime NOT NULL,
   `date_modified` datetime NOT NULL,
-  `content_md5` varchar(32) NOT NULL,
+  `content_md5` varchar(32) DEFAULT '',
   `content_type` varchar(500) DEFAULT NULL,
-  `fs_location` varchar(1000) DEFAULT NULL,
-  PRIMARY KEY (`bucket_name`,`object_key`)
+  `fs_location` varchar(100) DEFAULT NULL,
+  `content_size` int(11) unsigned DEFAULT '0',
+  PRIMARY KEY (`bucket_name`,`object_key`),
+  UNIQUE KEY `fs_location` (`fs_location`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
