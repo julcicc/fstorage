cf6bb65d184ebe24dd9ab36ae2a2c3c336b5e2ab
Julian Ciccale <julian@imaweb.es>
2017-01-05 11:57:29 +0100

    Fixed error message

--- fstorage_server/config.inc.php
+++ fstorage_server/config.inc.php
@@ -1,10 +1,10 @@
 <?php
 
-define('FSTORAGE_MYSQL_HOST', 'localhost');
-define('FSTORAGE_MYSQL_USER', 'root');
-define('FSTORAGE_MYSQL_PASS', '2702');
+define('FSTORAGE_MYSQL_HOST', 'imaweb');
+define('FSTORAGE_MYSQL_USER', 'crmauto');
+define('FSTORAGE_MYSQL_PASS', 'crmauto');
 define('FSTORAGE_MYSQL_DB', 'iw_storage');
-define('FSTORAGE_ROOT', '/tmp/fstorage');
+define('FSTORAGE_ROOT', '/home/julian/storage');
 
 define('FSTORAGE_ACCESS_USER', 'test');
 define('FSTORAGE_ACCESS_PASS', 'test');
